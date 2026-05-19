7f10b0a669f69dcd68dad003535521b0612b45cd
max-tantan <fatanjamterbang@gmail.com>
2026-05-19 07:52:54 +0700

    feat: update database password in knexfile and add new dependencies in package-lock

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: '',        // XAMPP defaultnya kosong
+    password: '123',        // XAMPP defaultnya kosong
     database: 'pos_db'
   }
 },
